c64b0372cf24a134bfca3ce6cab14ee7ff0d9af7
Ahamed Sajeeth <44955852+Ahamed-Sajeeth@users.noreply.github.com>
2019-02-20 19:28:09 +0530

    Add files via upload
    basic wordpress-theme

---
+++ sajeeth2/comments.php
@@ -0,0 +1,35 @@
+<?php if (post_password_required() ) {
+    return;
+    
+} ?>
+
+<div id="comments" class="comments-area">
+    <?php if (have_comments() ): ?>
+    <h3 class="comments-title">
+        <?php
+        printf(_nx('one comments on"%2$s"', '%1$s comments on "%2$s"', get_comments_number(),'comments title'),
+        number_format_i18n(get_comments_number()), get_the_title());
+        ?>
+    </h3>
+    <ul class="comment_list">
+        <?php
+                wp_list_comments(array(
+                    'short_ping' => true,
+                    'avatar_size' => 50,
+                    ));
+                ?>
+    </ul>
+    <?php endif; ?>
+<?php if ( ! comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments') ) : ?>
+    <p class="no-comments">
+        <?php _e('comments are closed.'); ?>
+    </p>
+        
+<?php endif; ?>
+<?php comment_form(); ?>
+</div>
+
+
+        
+        
+            
---
+++ sajeeth2/index.php
@@ -0,0 +1,35 @@
+
+   <?php get_header(); ?>
+      <div class="row">
+       <div class="col-sm-8 blog-main">
+           <?php 
+           if (have_posts()): while(have_posts()) : the_post();
+            get_template_part('content-single',get_post_format() ); 
+            endwhile;
+            ?>
+           <nav>
+            
+            <ul class="pager">
+                    <li><?php next_posts_link('previous');?></li>
+                    <li><?php previous_posts_link('next');?></li>
+       </ul>
+       </nav>
+            <?php endif;
+           ?>
+           
+     
+
+        </div>
+          <!-- /.blog-main -->
+        <?php get_sidebar(); ?>
+          
+      </div>
+
+        
+
+    <!-- /.row -->
+<?php get_footer(); ?>
+      
+   <!-- /.container -->
+
+   
